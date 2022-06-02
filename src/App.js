b3770dc2ee8c7b7c8941d91cd2f26cecdf5f7a2f
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-06-02 13:35:08 +0300

    api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import react from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid=735462ecb12cc75157889cb125f090a4";
+  // const api_key = "apikey=yt1dtI2ZeeNj6j2D7HNnVAXe9fhmj0UU&q=";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Weather App </h1>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>London</p>
+          </div>
+          <div className="temp">
+            <h1>25</h1>
+          </div>
+          <div className="description">
+            <p>clouds</p>
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
     </div>
   );
 }
